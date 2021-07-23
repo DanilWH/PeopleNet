79b392f5d0890ebf949b3d937a2553c473c2b6fd
DanilWH <danil-lomakin-02@mail.ru>
2021-07-23 19:28:01 +0300

    Moving to Angular - displaying and changing the data. (part 1)

---
+++ src/main/people-net-ui/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Message } from './message';
+import { MessageService } from './message.service';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+    public messages: Message[] = [];
+
+    constructor(private messageService: MessageService) { }
+
+    ngOnInit(): void {
+        this.getMessages();
+    }
+
+    public getMessages(): void {
+        this.messageService.getMessages().subscribe(
+            (response: Message[]) => {
+                this.messages = response;
+            },
+            (error: HttpErrorResponse) => {
+                alert(error.message);
+            }
+        )
+    }
+}
---
+++ src/main/people-net-ui/src/app/message.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Message } from "./message";
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class MessageService {
+    private apiServerUrl = environment.apiBaseUrl;
+
+    constructor(private http: HttpClient) { }
+
+    public getMessages(): Observable<Message[]> {
+        return this.http.get<Message[]>(`${this.apiServerUrl}/message`);
+    }
+
+    public getMessage(id: number): Observable<Message> {
+        return this.http.get<Message>(`${this.apiServerUrl}/message/${id}`);
+    }
+
+    public addMessage(message: Message): Observable<Message> {
+        return this.http.post<Message>(`${this.apiServerUrl}/message`, message);
+    }
+
+    public updateMessage(id: number, message: Message): Observable<Message> {
+        return this.http.put<Message>(`${this.apiServerUrl}/message/${id}`, message);
+    }
+
+    public deleteMessage(id: number): Observable<void> {
+        return this.http.delete<void>(`${this.apiServerUrl}/message/${id}`);
+    }
+}
